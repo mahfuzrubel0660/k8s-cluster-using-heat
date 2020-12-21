c45c94b08a8931bb29fb129e1aad2fe1cb8ee76b
Munna <munnaeebd@gmail.com>
2020-12-21 11:09:05 +0600

    Update slave.sh

--- slave.sh
+++ slave.sh
@@ -14,6 +14,22 @@ apt-get update
 
 apt-get install -y containerd.io=1.2.13-2 docker-ce=5:19.03.11~3-0~ubuntu-$(lsb_release -cs) docker-ce-cli=5:19.03.11~3-0~ubuntu-$(lsb_release -cs)
 apt-get install kubelet kubeadm kubectl -y
+
+mkdir /etc/docker
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2",
+  "storage-opts": [
+    "overlay2.override_kernel_check=true"
+  ]
+}
+EOF
+
 mkdir -p /etc/systemd/system/docker.service.d/
 systemctl daemon-reload
 systemctl enable docker
