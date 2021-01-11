7838539db7ae0037453c3b0bd2847ddd504e6940
Munna <munnaeebd@gmail.com>
2021-01-11 17:32:25 +0600

    Update slave.sh

--- slave.sh
+++ slave.sh
@@ -27,6 +27,14 @@ cat <<EOF | sudo tee /etc/docker/daemon.json
 }
 EOF
 
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+sysctl --system
+
 mkdir -p /etc/systemd/system/docker.service.d/
 systemctl daemon-reload
 systemctl enable docker
